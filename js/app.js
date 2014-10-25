c03a969ce0b47a529a7a1acb48f1ca4f29d6de51
Thang Dinh <thang@thangdc.com>
2014-10-25 15:39:30 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -39,6 +39,5 @@ app.config(function($routeProvider, $locationProvider) {
 		redirectTo:'/'
 	});
 	
-	$locationProvider.html5Mode(true);
     //$locationProvider.html5Mode(true).hashPrefix('!');
 });
