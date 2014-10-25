b1684661e8e03267969837ff54f253c5f4a430c3
Dinh Cong Thang <dinhcongthang113@hotmail.com>
2014-10-25 15:21:59 +0700

    Update

---
+++ js/app.js
@@ -0,0 +1,43 @@
+var app = angular.module('vietsofts', ['ngRoute', 'ngSanitize', 'firebase', 'vietsofts.controllers', 'vietsofts.services']);
+app.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+	.when('/', {
+		controller:'PostsCtrl',
+		templateUrl:'views/posts.html'
+	})
+	.when('/login', {
+		controller: 'AuthCtrl',
+		templateUrl: 'template/login.html',
+		resolve: {
+			user: function(Auth) {
+			  return Auth.resolveUser();
+			}
+		}
+	})
+	.when('/register', {
+		controller: 'AuthCtrl',
+		templateUrl: 'template/register.html',
+		resolve: {
+			user: function(Auth) {
+			  return Auth.resolveUser();
+			}
+		}
+	})
+	.when('/users/:userId', {
+		templateUrl: 'views/profile.html',
+		controller: 'ProfileCtrl'
+	})
+	.when('/posts', {
+		templateUrl: 'views/posts.html',
+		controller: 'PostsCtrl'
+    })
+	.when('/posts/:postId', {
+		templateUrl: 'views/showpost.html',
+		controller: 'PostViewCtrl'
+	})
+	.otherwise({
+		redirectTo:'/'
+	});
+
+    //$locationProvider.html5Mode(true).hashPrefix('!');
+});
