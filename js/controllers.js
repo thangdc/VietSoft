33b8c74d4f1a2e2ba1d64dc91429a8502ee58a59
thangdc <thang@thangdc.com>
2014-10-27 10:07:44 +0700

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -140,5 +140,5 @@ app.controller('NavCtrl', function ($scope, Auth, $location, Post, Helper) {
 });
 
 app.controller('MainCtrl', function($scope){
-	$scope.main = 'test';
+	$scope.data = 'test';
 });
