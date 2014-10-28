66e6973459169ac3044f3edfa417071e9bf522e9
Thang Dinh <thang@thangdc.com>
2014-10-28 13:33:49 +0700

    Update services.js

--- js/services.js
+++ js/services.js
@@ -178,8 +178,7 @@ app.factory('Main', function (){
 				{ Title: 'Dự án', Link: 'projects.html', Target: '', Active: path === "/projects.html" ? true : false },
 				{ Title: 'Blog', Link: 'http://www.thangdc.com', Target: '_blank', Active: path === "http://www.thangdc.com" ? true : false },
 				{ Title: 'Tạo QR Code', Link: '/qr-code-generator.html', Target: '', Active: path === "/qr-code-generator.html" ? true : false },
-				{ Title: 'Liên hệ', Link: 'contact.html', Target: '', Active: path === "/contact.html" ? true : false },
-				{ Title: 'Sitemap', Link: 'sitemap.html', Target: '', Active: path === "/sitemap.html" ? true : false }
+				{ Title: 'Liên hệ', Link: 'contact.html', Target: '', Active: path === "/contact.html" ? true : false }
 			];
 			return nav;
 		},
@@ -188,7 +187,8 @@ app.factory('Main', function (){
 				{ Title: 'Điều khoản sử dụng', Link: 'terms.html', Target: '', Active: path === "/terms.html" ? true : false, Minus: '-' },
 				{ Title: 'Chính sách', Link: 'policy.html', Target: '', Active: path === "/policy.html" ? true : false, Minus: '-' },
 				{ Title: 'Câu hỏi thường gặp', Link: 'FAQs.html', Target: '', Active: path === "/FAQs.html" ? true : false, Minus: '-' },
-				{ Title: 'Về chúng tôi', Link: 'about-vietsofts.html', Target: '', Active: path === "/about-vietsofts.html" ? true : false, Minus: '' }
+				{ Title: 'Về chúng tôi', Link: 'about-vietsofts.html', Target: '', Active: path === "/about-vietsofts.html" ? true : false, Minus: '-' },
+				{ Title: 'Sitemap', Link: 'sitemap.html', Target: '', Active: path === "/sitemap.html" ? true : false, Minus: '' }
 			];
 			return nav;
 		}
