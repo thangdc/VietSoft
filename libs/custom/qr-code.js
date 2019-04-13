79ba1db8e8c5f9b66cdadbd1209eb0e4e8520f7b
Thang Dinh <thang@thangdc.com>
2019-04-13 15:27:14 +0700

    Update qr-code.js

--- libs/custom/qr-code.js
+++ libs/custom/qr-code.js
@@ -90,7 +90,7 @@ $(document).ready(function () {
 					+ $('#contact_email').val() + ';EMAIL:' 
 		    			+ $('#contact_email1').val() + ';ADR:'
 					+ $('#contact_address').val() + ';NOTE:'
-					+ $('#textarea2').val() + ';;';
+					+ $('#textarea4').val() + ';;';
             generateqr(data);
         } else if (ind == 5) {
             data = 'geo:' + latitude + ',' + longitude;
